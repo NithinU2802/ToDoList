c2392533abcaf7f4abff219bb0596d46fbd3eee0
NithinU2802 <nithinu2810@gmail.com>
2025-01-14 23:43:11 +0530

    Initiate React + SpringBoot

---
+++ backend/ToDoList/src/test/java/com/learn/ToDoList/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.ToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
